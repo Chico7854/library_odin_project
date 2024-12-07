87bb28284096e795187881baecb3c628b7a18168
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-12-07 18:25:33 -0300

    Make the backdrop and modal close

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+const addBookButton = document.getElementById("new-book-button");
+const backdrop = document.getElementById("backdrop");
+const modalCard = document.getElementById("add-book-modal");
+
 const library = [];
 
-function Book(title, author, haveRead) {
+const Book = function(title, author, haveRead) {
     this.title = title;
     this.author = author;
-    this.haveRead = haveRead; 
+    this.haveRead = haveRead;
 }
 
-function addBookToLibrary(title, author, haveRead) {
+const addBookToLibrary = function(title, author, haveRead) {
     const book = new Book(title, author, haveRead);
 
     library.push(book);
 }
+
+const toggleBackdrop = function() {
+    backdrop.classList.toggle("visible");
+}
+
+const toggleModalCard = function() {
+    modalCard.classList.toggle("visible");
+}
+
+const addBookHandler = function() {
+    toggleBackdrop();
+    toggleModalCard();
+}
+
+const closeModalHandler = function() {
+    toggleBackdrop();
+    toggleModalCard();
+}
+
+addBookButton.addEventListener("click", addBookHandler);
+backdrop.addEventListener("click", closeModalHandler);
