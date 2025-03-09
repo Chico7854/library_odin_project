4a7b2e3e2af1b2d34b40b554d9525ed028da9b52
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2025-03-09 00:39:00 -0300

    Make Sign up page front end

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 
-const routes = require("./routes/main");
+const mainRoutes = require("./routes/main");
+const authRoutes = require("./routes/auth");
 
 const MONGODB_URI = "mongodb+srv://lacus7854:dl2RZ1UdK4Xd$9N@librarycluster.ncdub.mongodb.net/?retryWrites=true&w=majority&appName=libraryCluster"
 const app = express();
@@ -13,7 +14,8 @@ app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+app.use(mainRoutes);
+app.use(authRoutes);
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
