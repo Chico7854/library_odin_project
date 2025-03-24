2363f20651e9a1b761d61b2d9ff7978c1df3c866
Lucas T <160812489+Chico7854@users.noreply.github.com>
2025-03-24 15:37:21 -0300

    Change post from fetch to form

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 app.use(mainRoutes);
